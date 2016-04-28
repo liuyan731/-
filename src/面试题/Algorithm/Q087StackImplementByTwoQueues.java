4116e79eb3472b3ce9f3c13ff4dca67083393069
liuyan <liuyan990731@gmail.com>
2016-04-28 10:48:19 +0800

    Add Readme

---
+++ src/面试题/Algorithm/Q087StackImplementByTwoQueues.java
@@ -0,0 +1,12 @@
+/*
+ * StackImplementByTwoQueues.java
+ *
+ *  Created on: 2016年4月28日
+ *      Author: liuyan
+ */
+
+package 面试题.Algorithm;
+
+public class Q087StackImplementByTwoQueues {
+	
+}
---
+++ src/面试题/Algorithm/Q088QueueImplementByTwoStacks.java
@@ -0,0 +1,12 @@
+/*
+ * QueueImplementByTwoStacks.java
+ *	使用两个栈来实现一个队列
+ *  Created on: 2016年4月28日
+ *      Author: liuyan
+ */
+
+package 面试题.Algorithm;
+
+public class Q088QueueImplementByTwoStacks {
+	// 使用两个栈来实现一个队列，思路：插入操作使用Stack1来完成，删除操作使用Satck2
+}
